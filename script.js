814910c37fbba0117b6f41a3421cbc00964e1fc4
Roger-afk-netizen <rogertamayo2003@outlook.es>
2025-02-25 09:51:11 -0500

    cargando

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function obtenerMensaje() {
     try {
         const respuesta = await fetch('data.json');
         const datos = await respuesta.json();
-        document.getElementById('mensaje').innerText = datos.message;
+        document.getElementById('mensajeeee').innerText = datos.message;
     } catch (error) {
         console.error('Error cargando el mensaje a2sdaaa :', error);
     }
